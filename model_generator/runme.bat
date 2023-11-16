dfe6e08fb28dde2081e285c97949e5c53cfd2b3d
Pozytyw <tvpozytyw@gmail.com>
2023-11-16 22:17:09 +0100

    backup

--- model_generator/runme.bat
+++ model_generator/runme.bat
@@ -3,11 +3,15 @@ rd /s/q src
 java -jar openapi-generator-cli.jar ^
     generate -g dart-dio ^
     -o src ^
+    -t templates ^
     --skip-validate-spec ^
     -c configuration.json ^
     --input-spec-root-directory apis ^
     --model-name-suffix DTO ^
-    --global-property modelTests=false,modelDocs=false,models
+    --global-property modelTests=false,modelDocs=false
 
-rd /s/q ..\lib\infrastructure\rit\model
+@REM xcopy src\lib ..\lib /s /e /h
+@REM cd ..
+@REM flutter pub run build_runner build
+@REM cd model_generator
 pause
