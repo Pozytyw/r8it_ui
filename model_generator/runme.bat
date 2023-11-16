54f6500feda20dfc828ed05c395d95e0024e02a1
Pozytyw <tvpozytyw@gmail.com>
2023-11-16 21:47:59 +0100

    fixes

--- model_generator/runme.bat
+++ model_generator/runme.bat
@@ -1,3 +1,13 @@
 @echo off
-wsl -d Ubuntu ./runme
+rd /s/q src
+java -jar openapi-generator-cli.jar ^
+    generate -g dart-dio ^
+    -o src ^
+    --skip-validate-spec ^
+    -c configuration.json ^
+    --input-spec-root-directory apis ^
+    --model-name-suffix DTO ^
+    --global-property models
+
+rd /s/q ..\lib\infrastructure\rit\model
 pause
