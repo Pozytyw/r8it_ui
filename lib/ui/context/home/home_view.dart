93252f33789dc42823d01219fbb7446470821d38
Pozytyw <tvpozytyw@gmail.com>
2023-12-13 15:03:58 +0100

    Custom action bar

--- lib/ui/context/home/home_view.dart
+++ lib/ui/context/home/home_view.dart
@@ -1,52 +1,252 @@
 import 'package:flutter/material.dart';
-import 'package:r8it/domain/rit_repository.dart';
-import 'package:r8it/storage/vault.dart';
-import 'package:r8it/ui/app_router.dart';
-import 'package:r8it/ui/widget/page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:r8it/ui/widget/refreshable.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
+  static final spacer = SizedBox.fromSize(size: const Size(8, 8));
+
   @override
   Widget build(BuildContext context) {
-    // var l10n = AppLocalizations.of(context);
-    // var theme = Theme.of(context);
-    // var colorScheme = theme.colorScheme;
-    return AppPage(
-      body: Center(
+    var l10n = AppLocalizations.of(context);
+    var theme = Theme.of(context);
+    var colorScheme = theme.colorScheme;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: searchAppBar(context),
+      body: RefreshableWidget(
+        onRefresh: Future.value,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ww(context),
+              spacer,
+              ww(context),
+              spacer,
+              ww(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget ww(BuildContext context) {
+    var l10n = AppLocalizations.of(context);
+    var theme = Theme.of(context);
+    var colorScheme = theme.colorScheme;
+    var style = theme.textTheme.bodyLarge;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: colorScheme.onSurface,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Text('Hi'),
-            UserInfo(),
-            ElevatedButton(
-                onPressed: () => _onPressed(context), child: Text('Log out'))
+            Row(
+              children: [
+                Icon(Icons.my_location, size: 16),
+                spacer,
+                Text('location name')
+              ],
+            ),
+            spacer,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: colorScheme.onSecondary,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      width: 48,
+                      height: 48,
+                    ),
+                    spacer,
+                    Text('First name'),
+                  ],
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: colorScheme.onSecondary,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  width: 48,
+                  height: 48,
+                  alignment: Alignment.center,
+                  child: Text(
+                    '7',
+                    style: style,
+                  ),
+                )
+              ],
+            ),
+            spacer,
+            Row(
+              children: [
+                Expanded(
+                  child: image(),
+                ),
+              ],
+            ),
+            spacer,
+            Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+            spacer,
+            Row(children: [
+              Text('#food'),
+            ]),
+            spacer,
+            Row(children: [
+              Text('04:55 PM'),
+              Text('23/10/2023'),
+            ]),
+            spacer,
+            Row(
+              children: [
+                Icon(Icons.message),
+                Icon(Icons.share),
+              ],
+            )
           ],
         ),
       ),
     );
   }
 
-  void _onPressed(BuildContext context) {
-    Vault.instance()
-        .then((value) => value.removeAuthToken())
-        .then((value) => AppRouter.goLoginPageName(context));
-  }
-}
-
-class UserInfo extends StatelessWidget {
-  const UserInfo({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<String>(
-      future: _fetchUserInfo(),
-      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-        return Text(snapshot.data ?? '');
-      },
+  Widget image() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Image.asset(
+        'assets/images/coffe.jpg',
+        fit: BoxFit.fitWidth,
+      ),
     );
   }
 
-  Future<String> _fetchUserInfo() {
-    return RitRepository.instance().userInfo().then((info) => info.username);
+  PreferredSizeWidget searchAppBar(BuildContext context) {
+    var l10n = AppLocalizations.of(context);
+    var theme = Theme.of(context);
+    var colorScheme = theme.colorScheme;
+    var style = theme.textTheme.titleLarge;
+    var bottom = Xxx();
+    return AppBar(
+      toolbarHeight: bottom.preferredSize.height + 32,
+      titleSpacing: 16,
+      title: FilledButton(
+        style: FilledButton.styleFrom(
+          textStyle: style,
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+        ),
+        onPressed: () {},
+        child: Row(
+          children: [
+            Icon(
+              Icons.search_outlined,
+              color: style?.color,
+            ),
+            spacer,
+            Text('Search'),
+          ],
+        ),
+      ),
+      // title: TextField(
+      //   decoration: InputDecoration(
+      //     contentPadding: EdgeInsets.symmetric(horizontal: 16),
+      //     floatingLabelBehavior: FloatingLabelBehavior.never,
+      //     border: OutlineInputBorder(
+      //       borderSide: BorderSide.none,
+      //       borderRadius: BorderRadius.circular(50),
+      //     ),
+      //     filled: true,
+      //     label: Row(
+      //       children: [
+      //         Icon(Icons.search_outlined, color: style?.color,),
+      //         spacer,
+      //         Text('Search'),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      flexibleSpace: const SizedBox.shrink(),
+      bottom: bottom,
+    );
+  }
+}
+
+class Xxx extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    var spacer = SizedBox.fromSize(size: const Size(8, 0));
+    return Container(
+      alignment: Alignment.topLeft,
+      height: preferredSize.height,
+      padding: const EdgeInsets.only(left: 16),
+      child: Row(
+        children: [
+          TagWidget('Food'),
+          spacer,
+          TagWidget('Coffee'),
+          spacer,
+          TagWidget('Books'),
+          spacer,
+          TagWidget('Movies'),
+          spacer,
+          TagWidget('...'),
+        ],
+      ),
+    );
+  }
+
+  /*todo: how to calculate it?*/
+  @override
+  Size get preferredSize => Size.fromHeight(32);
+}
+
+class TagWidget extends StatelessWidget {
+  final String name;
+
+  const TagWidget(this.name, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var l10n = AppLocalizations.of(context);
+    var theme = Theme.of(context);
+    var colorScheme = theme.colorScheme;
+    var style = ElevatedButton.styleFrom(
+      padding: EdgeInsets.all(12),
+      minimumSize: const Size(0, 0),
+    );
+
+    return ElevatedButton(
+      onPressed: () {},
+      style: style,
+      child: Text(name),
+    );
+  }
+}
+
+class TextBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: TextField(
+        decoration:
+            InputDecoration(border: InputBorder.none, hintText: 'Search'),
+      ),
+    );
   }
 }
--- lib/ui/widget/page.dart
+++ lib/ui/widget/page.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:r8it/storage/preferences.dart';
-import 'package:r8it/ui/state/simple_events.dart';
 
 class AppPage extends StatelessWidget {
   final EdgeInsets bodyPadding;
   final Widget? appBarTitleWidget;
   final Widget? body;
+  final List<Widget>? actions;
 
   const AppPage({
     this.appBarTitleWidget,
     super.key,
     this.body,
     this.bodyPadding = const EdgeInsets.all(8.0),
+    this.actions,
   });
 
   @override
@@ -23,62 +21,12 @@ class AppPage extends StatelessWidget {
       appBar: AppBar(
         title: appBarTitleWidget,
         elevation: 0,
-        actions: const [LanguageSelector()],
+        actions: actions,
       ),
       body: Padding(
         padding: bodyPadding,
         child: body,
       ),
     );
-  }
-}
-
-class LanguageSelector extends StatefulWidget {
-  const LanguageSelector({super.key});
-
-  @override
-  State<LanguageSelector> createState() => _LanguageSelectorState();
-}
-
-class _LanguageSelectorState extends State<LanguageSelector> {
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<LocaleChangeState, Locale>(
-      builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: DropdownButton(
-            value: state.languageCode,
-            items: supportedLanguagesAsDropdownMenuItem(),
-            borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-            onChanged: (value) {
-              setState(() {
-                if (value != null && value.isNotEmpty) {
-                  context.read<LocaleChangeState>().emit(Locale(value));
-                }
-              });
-            },
-          ),
-        );
-      },
-    );
-  }
-
-  List<DropdownMenuItem<String>> supportedLanguagesAsDropdownMenuItem() {
-    return AppLocalizations.supportedLocales
-        .map((e) => e.languageCode)
-        .map((e) =>
-        DropdownMenuItem<String>(value: e, child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(e),
-        )))
-        .toList();
-  }
-
-  Future<String> currentLanguageFuture() {
-    return GlobalPreferences.instance().then((it) =>
-    it
-        .getPreferableLocale()
-        .languageCode);
   }
 }
